2fd36b4ab7c64a34aec90c42d5387fd54c101557
giannhskod <giannhskontogewrgos@gmail.com>
2019-12-18 10:08:24 +0200

    Final Refactoring

--- R/code/data.R
+++
@@ -1,2 +0,0 @@
-data.flights.outbout = sqlite_to_df(db = "data/db.sqlite", query = query.flights.outbound)
-data.delays.security.by_destination = sqlite_to_df(db = "data/db.sqlite", query = query.delays.security.by_destination)
